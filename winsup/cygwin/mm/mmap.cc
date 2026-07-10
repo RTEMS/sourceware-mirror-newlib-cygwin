picolibc/picolibc

commit 61522196c71593da09572fce9af9e0d7dad61bc3

* Merge in cygwin-64bit-branch.

winsup/cygwin/cygwin.din
@@ -678,8 +678,8 @@ get_avphys_pages SIGFE
 get_current_dir_name SIGFE
 get_nprocs SIGFE
 get_nprocs_conf SIGFE
-get_osfhandle SIGFE
-_get_osfhandle = get_osfhandle SIGFE
+_get_osfhandle SIGFE
+get_osfhandle = _get_osfhandle SIGFE
 get_phys_pages SIGFE
 getaddrinfo = cygwin_getaddrinfo SIGFE
 getc SIGFE

RTEMS/sourceware-mirror-newlib-cygwin

commit 383618c8902f5a14b258e1ffa3288a4e19780a76

Cygwin: Fix error return for madvise()
Currently madvise() and posix_madvise() are wired together as one
function: the latter.  But their error returns should be different.
Make madvise a first-class export in cygwin.din.

v2: Create madvise_worker() and have madvise() and posix_madvise()
    call it, then handling their error returns compliant to POSIX.
    Add a release note for 3.7.0.

Reported-by: Christian Franke <Christian.Franke@t-online.de>
Addresses: https://cygwin.com/pipermail/cygwin/2026-July/259872.html
Signed-off-by: Mark Geisert <mark@maxrnd.com>
Fixes: 61522196c715 (* Merge in cygwin-64bit-branch.)
Reviewed-by: Takashi Yano, Christian Franke

winsup/cygwin/cygwin.din
@@ -951,7 +951,7 @@ lseek SIGFE
 lsetxattr SIGFE
 lstat SIGFE
 lutimes SIGFE
-madvise = posix_madvise SIGFE
+madvise SIGFE
 makecontext NOSIGFE
 mallinfo SIGFE
 malloc SIGFE

winsup/cygwin/mm/mmap.cc
@@ -1422,8 +1422,8 @@ munlock (const void *addr, size_t len)
   return ret;
 }
 
-extern "C" int
-posix_madvise (void *addr, size_t len, int advice)
+static int
+madvise_worker (void *addr, size_t len, int advice)
 {
   int ret = 0;
   /* Check parameters. */
@@ -1514,6 +1514,26 @@ posix_madvise (void *addr, size_t len, int advice)
       break;
     }
 out:
+  return ret;
+}
+
+extern "C" int
+madvise (void *addr, size_t len, int advice)
+{
+  int ret = madvise_worker (addr, len, advice);
+  if (ret > 0)
+    {
+      set_errno (ret);
+      ret = -1;
+    }
+  syscall_printf ("%R = madvise(%p, %lu, %d)", ret, addr, len, advice);
+  return ret;
+}
+
+extern "C" int
+posix_madvise (void *addr, size_t len, int advice)
+{
+  int ret = madvise_worker (addr, len, advice);
   syscall_printf ("%d = posix_madvise(%p, %lu, %d)", ret, addr, len, advice);
   return ret;
 }

winsup/cygwin/release/3.7.0
@@ -25,3 +25,11 @@ What's new:
 - Now, a Cygwin process started from a non‑Cygwin process on a pseudo console
   runs on a pty rather than on the console device originating from the pseudo
   console.
+
+Fixes:
+------
+
+- Error return logic for madvise() is now separated from posix_madvise().
+  If madvise() errors, it returns -1 with errno set. If posix_madvise()
+  errors, it returns an error number without changing errno.
+  Addresses: https://cygwin.com/pipermail/cygwin/2026-July/259872.html